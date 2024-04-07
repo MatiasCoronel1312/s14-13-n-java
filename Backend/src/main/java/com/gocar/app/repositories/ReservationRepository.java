10266b9d63b81b266cc43e35ee2f6b550cb9e0ba
Raul Gomez <gomez.raul.1978.rg@gmail.com>
2024-04-07 18:28:32 -0300

    delete folder backend from frontend

--- Backend/src/main/java/com/gocar/app/repositories/ReservationRepository.java
+++ Backend/src/main/java/com/gocar/app/repositories/ReservationRepository.java
@@ -1,14 +1,13 @@
 package com.gocar.app.repositories;
+
+import com.gocar.app.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
-import com.gocar.app.models.Reservation;
-import com.gocar.app.models.User;
-
 import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-
-public interface ReservationRepository extends JpaRepository<Reservation,Long> {
-
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+     Optional<UserDetails> findByEmail(String email);
 }
