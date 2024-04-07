39e50d84dc866d5b849056254ba3eff742cc4bb2
Raul1978x <108808330+Raul1978x@users.noreply.github.com>
2024-04-07 04:16:33 -0300

    feat: new reservation

--- Frontend/src/components/CarCarousel/Carrusel.tsx
+++ Frontend/src/components/CarCarousel/Carrusel.tsx
@@ -69,11 +69,7 @@ export default function Carrusel() {
 
   return (
     <>
-
-
       <section className="h-full max-w-full">
-
-
         <div className="mb-8">
           <h1 className="text-center font-sans font-bold text-text text-2xl">
             Conocé nuestra flota
