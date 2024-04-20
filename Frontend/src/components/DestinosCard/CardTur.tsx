be89f280eb4ae38017af9a1a403098269e103f1c
[Lauto22] <[lauto_22@hotmail.com]>
2024-04-20 00:09:35 -0300

    aun no rompi nada

--- Frontend/src/components/DestinosCard/CardTur.tsx
+++ Frontend/src/components/DestinosCard/CardTur.tsx
@@ -1,37 +1,30 @@
 import { useNavigate } from "react-router-dom";
 import ButtonTag from "./ButtonTag";
 import { DataDestination } from "./DataDestination.interface";
-import { useEffect } from "react";
 
 interface Prop{lugar:DataDestination}
 
 function CardTur({lugar}:Prop){
     const navigator = useNavigate();
-    useEffect(() => {
-
-        window.scrollTo({
-          top:0,
-          behavior:'smooth'
-        })
-    }, []);
+    
     
     const handleClick =(id:number)=>{
-         navigator(`/DestinationDetail/${id}`)
+            navigator(`/DestinationDetail/${id}`)
         
     };
     return (
-        <div onClick={()=>{handleClick(lugar.id)}} className="flex rounded-lg border-black border-2 h-[256px] w-[540px] mb-2" key={lugar.id}>
-            <div className=" rounded-l-lg w-1/3  h-full">
-                <img className="rounded-l-lg w-full h-full" src={lugar.images[1]} alt={lugar.title}/>
+        <div onClick={()=>{handleClick(lugar.id)}} className="flex rounded-lg border-text border-[1px] h-[256px] w-[540px] mb-2 bg-white shadow-lg cursor-pointer" key={lugar.id}>
+            <div className=" rounded-l-lg w-1/3  h-full overflow-hidden" >
+                <img className="rounded-l-lg w-full h-full object-cover" src={lugar.images[1]} alt={lugar.title}/>
             </div>
             <div className="flex flex-col w-2/3 ">
                 <div className="gap-5 p-5">
                     <h3 className="text-xl font-semibold mb-2 text-accent ">{lugar.city}</h3>
                     <p className="">{lugar.subtitle}</p>
                 </div>
-                <div>{
+                <div className="flex justify-start flex-wrap gap-2 px-5">{
                     lugar.tag.map(o=>(
-                        <ButtonTag tag={o}/>
+                        <ButtonTag  tag={o}/>
                     ))
                     }
                 </div>
--- Frontend/src/pages/TipsTuristicos.tsx
+++ Frontend/src/pages/TipsTuristicos.tsx
@@ -1,15 +1,63 @@
+import { useEffect, useState } from "react";
 import CardTur from "../components/DestinosCard/CardTur";
 import lugares from "../components/DestinosCard/DataDestinos";
+import ButtonTag from "../components/DestinosCard/ButtonTag";
+import { FaSearch } from "react-icons/fa";
+
 
 const TipsTuristicos = () => {
+  const [filtro, setFiltro] = useState("")
+  const filtros =["Fiestas","Aventura","Historia","Gastronomia","Naturaleza"]
+  useEffect(() => {
+
+    window.scrollTo({
+        top:0,
+        behavior:'smooth'
+    })
+}, []);
+  const lugaresFiltrados = filtro.length<2?lugares: lugares.filter(lugar => 
+    
+    {
+      const tagMinuscula = lugar.tag.map(dato=>dato.toLowerCase())      
+      return tagMinuscula.some(name=>name.includes(filtro.toLowerCase()))
+    });
+  const handleChange =(e: React.ChangeEvent<HTMLInputElement>)=> {
+    setFiltro(e.target.value)
+  }
   return (
     <div className='w-full bg-background flex justify-center'>
       <div className="h-full lg:w-[85%] md:w-[90%] ">
-        <div>TipsTuristicos</div>
-
+        <h1 className="text-accent font-bold text-3xl text-center p-5">Tips de viajes</h1>
+        <h2 className="text-2xl text-center pb-5"> Vení a inspirarte para tu próximo destino</h2>
+        <div className="w-[85%] h-full flex flex-col item-center mx-auto">
+        <p>Busque por categoría o palabra clave:</p>
+        <div className="flex justify-start gap-5 items-center">
+          <div className="relative w-[860px] h-[40px]">
+            <FaSearch className="absolute left-3 top-3 text-[#707070]"/>
+            <input 
+              type="text" 
+              className="w-full p-3 pl-10 h-full border-[#707070] border-2 rounded-lg"
+              placeholder="Ej: Aventura"
+              value={filtro}
+              onChange={handleChange}/>
+          </div>
+          <div>
+            <a className="text-accent cursor-pointer hover:underline" onClick={()=>{setFiltro("")}}>
+              Limpiar filtro
+            </a>
+          </div>
+        </div>
+        <div className="flex justify-start ml-5 flex-row gap-5 py-5">
+          {
+            filtros.map((filter)=>
+              <ButtonTag  setFiltro={setFiltro} tag={filter} />
+            )
+          }
+        </div>
+        </div>
         <div className="flex flex-wrap p-4 mb-4 justify-between ">
           {
-            lugares.map(lugar=>(
+            lugaresFiltrados.map(lugar=>(
               <CardTur lugar={lugar}/>
             ))
           }
