d1b21e7891f9dc127d8311e21fdcb88252fe3e7d
Brian <diazbrian47@gmail.com>
2024-04-22 20:51:27 -0500

    hotfix: change data in some vehicles

--- Backend/src/main/resources/db/migration/V4__INSERT_INTO_VEHICLES.sql
+++ Backend/src/main/resources/db/migration/V4__INSERT_INTO_VEHICLES.sql
@@ -1,11 +1,11 @@
-INSERT INTO vehicles (brand, model, model_year, engine_size, passengers, image, price, stock, category)
+INSERT INTO vehicles (brand, model, model_Year, engine_Size, passengers, image, price, stock, category)
 VALUES
-    ('Toyota', 'Corolla', 2024, 1.6, 5, 'https://i.postimg.cc/7hRHgdNn/Toyota-Corolla.png', 62, 5, 'SEDAN_PREMIUM'),
-    ('Dodge', 'Challenger', 2024, 3.6, 4, 'https://i.postimg.cc/zfwG9dJq/Dodge-Challenger.png', 82, 0, 'ECONOMIC'),
-    ('Chevrolet', 'Tahoe', 2024, 5.3, 7, 'https://i.postimg.cc/Bvfv7Wfh/Chevrolet-Tahoe.png', 159, 3, 'ECONOMIC'),
-    ('Nissan', 'Versa', 2024, 1.6, 5, 'https://i.postimg.cc/5yy9Qb8t/Nissan-Versa.png', 70, 5, 'MINIVAN'),
-    ('Ford', 'Transit', 2024, 3.5, 15, 'https://i.postimg.cc/mD82rtzq/Ford-Transit.png', 210, 5, 'SEDAN'),
-    ('Chevrolet', 'Express', 2024, 2.8, 2, 'https://i.postimg.cc/pLFTBW4h/Chevrolet-Express.png', 130, 0, 'SPORT'),
-    ('Ford', 'Mustang Convertible', 2024, 5.0, 4, 'https://i.postimg.cc/LXH8RzXL/Ford-Mustang-Convertible.png', 310, 5, 'SUV'),
-    ('Audi', 'A5', 2024, 2.0, 5, 'https://i.postimg.cc/J4g0JD6C/AudiA5.png', 140, 5, 'TRUCK'),
-    ('Toyota', 'Prius', 2024, 1.8, 5, 'https://i.postimg.cc/3rfx50H0/Toyota-Prius.png', 80, 5, 'VAN');
+    ('Toyota', 'Corolla', 2024, 1.6, 5, 'https://i.postimg.cc/7hRHgdNn/Toyota-Corolla.png', 62.0, 5, 'SEDAN_PREMIUM'),
+    ('Dodge', 'Challenger', 2024, 3.6, 4, 'https://i.postimg.cc/zfwG9dJq/Dodge-Challenger.png', 82.0, 0, 'SPORT'),
+    ('Chevrolet', 'Tahoe', 2024, 5.3, 7, 'https://i.postimg.cc/Bvfv7Wfh/Chevrolet-Tahoe.png', 159.0, 3, 'SUV'),
+    ('Nissan', 'Versa', 2024, 1.6, 5, 'https://i.postimg.cc/5yy9Qb8t/Nissan-Versa.png', 70.0, 5, 'SEDAN_PREMIUM'),
+    ('Ford', 'Transit', 2024, 3.5, 15, 'https://i.postimg.cc/mD82rtzq/Ford-Transit.png', 210.0, 5, 'VAN'),
+    ('Chevrolet', 'Express', 2024, 2.8, 2, 'https://i.postimg.cc/pLFTBW4h/Chevrolet-Express.png', 130.0, 0, 'VAN'),
+    ('Ford', 'Mustang Convertible', 2024, 5.0, 4, 'https://i.postimg.cc/LXH8RzXL/Ford-Mustang-Convertible.png', 310.0, 5, 'SPORT'),
+    ('Audi', 'A5', 2024, 2.0, 5, 'https://i.postimg.cc/J4g0JD6C/AudiA5.png', 140.0, 5, 'SEDAN_PREMIUM'),
+    ('Toyota', 'Prius', 2024, 1.8, 5, 'https://i.postimg.cc/3rfx50H0/Toyota-Prius.png', 80.0, 5, 'COMPACT_HATCHBACK');
