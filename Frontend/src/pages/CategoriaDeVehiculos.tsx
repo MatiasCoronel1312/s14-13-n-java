a6728d7d3f16c6b2ea7279e6dc57237d36aabe21
albert <angelalberto545@gmail.com>
2024-04-24 00:06:28 -0400

    intento de responsive para front car

--- Frontend/src/pages/CategoriaDeVehiculos.tsx
+++ Frontend/src/pages/CategoriaDeVehiculos.tsx
@@ -4,24 +4,16 @@ import NuevaReservaPrueba from "../components/NuevaReserva/NuevaReservaPrueba";
 
 import { useAppSeletor } from "../redux/store";
 
-//https://gocarapp.onrender.com/api/vehicles/all
-
 const CategoriaDeVehiculos = () => {
   const dataReduces = useAppSeletor((state) => state);
 
   const dataReserve = dataReduces.dataReserve.dataReserve;
-  const isFromReserva = dataReserve.fechaRetiro !== undefined;
-
-  // navigate("/finalizar-pago");
+  const isFromReserva = dataReserve?.fechaRetiro !== undefined;
 
   return (
-    <div className='w-full h-full bg-background flex justify-center px-3'>
+    <div className="w-full h-full bg-background flex justify-center px-3">
       <div className="lg:w-[85%] md:w-[90%] w-full h-full flex flex-col items-center">
-        {isFromReserva ? (
-          <TimeLine posicion={2} />
-        ) : (
-          <NuevaReservaPrueba />
-        )}
+        {isFromReserva ? <TimeLine posicion={2} /> : <NuevaReservaPrueba />}
         <SelectForCategory />
       </div>
     </div>
